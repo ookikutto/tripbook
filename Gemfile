0897ace62b038a02845a53aba502c87d585e29e1
Kien Do <kiendo95@gmail.com>
2017-04-18 23:43:20 +0700

    Use I18n for japanese

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem 'instagram'
 gem 'omniauth-google-oauth2'
 gem 'google-api-client'
 
+gem 'rails-i18n'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ?
+        locale :
+        I18n.default_locale
+  end
 end

df0bcedbc6eada006d3362d337e1d5010ae7aa93
Kien Do <kiendo95@gmail.com>
2017-04-21 15:46:51 +0700

    Seed a story (include a new user, new place)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create email: '1@1.com', password: '111111'
+Place.create name: 'New York'
+Story.create title: 'The best trip to New York, ever', place: Place.last, user: User.last

a3b95ae1058bacffafd36e19592986f6742d9c70
Kien Do <kiendo95@gmail.com>
2017-05-16 11:35:29 +0700

    show place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
   def show
     @place = Place.find_by id: params[:id]
+    @lastest = Story.where(place: @place).order(created_at: :desc)
+    @hostest = Story.where(place: @place).sort_by { |story| -story.unique_impression_count }
   end
 end

278569115ebb3ef8b3ab0e1408b633bde0b5e907
Kien Do <kiendo95@gmail.com>
2017-05-18 10:47:16 +0700

    Fix UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     @lastest = @user.stories.order(created_at: :desc)
     @hostest = @user.stories.sort_by {|story| -story.unique_impression_count}
+    @favorites = @user.loved_stories.order created_at: :desc
   end
 end
